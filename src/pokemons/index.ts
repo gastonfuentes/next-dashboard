f592154bec238d7aac94365db9847105f5d64bcc
gastonfuentes <gastonnicolasfuentes@gmail.com>
2024-07-19 20:56:27 -0300

    pokemon mostrados por id

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -3,4 +3,5 @@ export { PokemonGrid } from './components/PokemonGrid'
 
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon-response';
+export type { Pokemon } from './interfaces/pokemon';
 
